333c64f3506b66790ddc9a62314e20da803e47df
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-03-10 14:00:56 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,6 +3,6 @@ INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES ( "Rowd
 INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "female", "brown", "angry", 1 );
 INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES ( "Sergeant Brown", 8, "male", "brown", "angry", 1);
 INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES ( "Melissa", 7, "female", "brown", "happy", 1);
-INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES ( "Grinch", 9, "male", black, hungry, 1);
-INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES ( Wendy, 11, female, brown, hungry, 1);
+INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES ( "Grinch", 9, "male", "black", "hungry", 1);
+INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES ( "Wendy", 11, female, brown, hungry, 1);
 INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES ( NULL, 28, male, brown, killer, 1);
