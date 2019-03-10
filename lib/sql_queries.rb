1b8b1c2b0f7cc040ef3a69606ff415c6c10759af
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-03-10 14:08:24 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNT(color) FROM bears ORDER BY "
+  "SELECT COUNT(color) FROM bears LIMIT 1"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
