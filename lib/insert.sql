e1a81a45a3e2f07caf8b0a1af55b2be82beba09d
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-03-10 14:00:40 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 4, "male", "brown", "sleepy", 1);
 INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES ( "Rowdy", 5, "male", "black", "sleepy", 1);
 INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "female", "brown", "angry", 1 );
-INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES ( "Sergeant Brown", 8, "male", brown, angry, 1);
-INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES ( Melissa, 7, female, brown, happy, 1);
-INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES ( Grinch, 9, male, black, hungry, 1);
+INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES ( "Sergeant Brown", 8, "male", "brown", "angry", 1);
+INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES ( "Melissa", 7, "female", "brown", "happy", 1);
+INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES ( "Grinch", 9, male, black, hungry, 1);
 INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES ( Wendy, 11, female, brown, hungry, 1);
 INSERT INTO bears ( name, age, gender, color, temperament, alive) VALUES ( NULL, 28, male, brown, killer, 1);
