7b3ac2ec6d6cd81628973002d5567baa0108980e
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-03-10 13:48:35 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (0, Mr.Chocolate, 4, male, brown, sleepy, 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, Rowdy, 5, male, black, sleepy, 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, Tabitha, 3, female, brown, angry, 1 );
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3. Sergeant Brown, 8, male, brown, angry, 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, Melissa, 7, female, brown, happy, 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, Grinch, 9, male, black, hungry, 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, Wendy, 11, female, brown, hungry, 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, NULL, 28, male, brown, killer, 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (Mr.Chocolate, 4, male, brown, sleepy, 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ( Rowdy, 5, male, black, sleepy, 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (Tabitha, 3, female, brown, angry, 1 );
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ( Sergeant Brown, 8, male, brown, angry, 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ( Melissa, 7, female, brown, happy, 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ( Grinch, 9, male, black, hungry, 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ( Wendy, 11, female, brown, hungry, 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ( NULL, 28, male, brown, killer, 1);
