357c30e0f1ab3363ab628fa335d47940312c7437
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-03-10 13:48:40 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE bears (id INTEGER, name TEXT, age INTEGER, gender TEXT, color, TEXT, temperament TEXT, alive INTEGER);
+CREATE TABLE bears (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, gender TEXT, color, TEXT, temperament TEXT, alive INTEGER);
